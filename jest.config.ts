df47b3c7423e9ba191fb35292dede67e3548794f
Natalie Springer <springer5296@gmail.com>
2025-02-13 17:03:55 -0600

    Remove files

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,6 @@ const config: Config = {
 	collectCoverage: true,
 	preset: "jest-expo",
 	setupFiles: ["<rootDir>/jest.setup.js"],
-	setupFilesAfterEnv: ["<rootDir>/jestSetupAfterEnv.ts"],
 	transformIgnorePatterns: [
 		"node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)"
 	],
