04d490ff14be4c66e97540ab0ea11507279ab297
Natalie Springer <springer5296@gmail.com>
2025-02-16 11:11:24 -0600

    Create publish steps

---
+++ update-main.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const packageJsonPath = path.resolve(__dirname, 'package.json');
+const packageJson = require(packageJsonPath);
+
+packageJson.main = './index.ts';
+
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf8');
+console.log('Updated main field in package.json to "./index.ts"');
