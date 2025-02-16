2ee85eecfc7f594806e6f0548aa7940e74d15bcd
Natalie Springer <springer5296@gmail.com>
2025-02-16 10:44:15 -0600

    Add scroll and animation

--- components/LayoutContainer/index.tsx
+++ components/LayoutContainer/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { View } from "react-native";
+import { SafeAreaView } from "react-native";
 import { LayoutContainerProps } from "./types";
 import { stylesheet } from "./styles";
 import { useStyles } from "react-native-unistyles";
+import Animated, { FadeInUp } from "react-native-reanimated";
 
 export const LayoutContainer = ({ children }: LayoutContainerProps) => {
 	const { styles } = useStyles(stylesheet);
 
-	return <View style={styles.container}>{children}</View>;
+	return (
+		<SafeAreaView>
+			<Animated.ScrollView entering={FadeInUp} style={styles.container}>
+				{children}
+			</Animated.ScrollView>
+		</SafeAreaView>
+	);
 };
